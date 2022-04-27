47115a8d4a435d1375622814ec5af87bd00ad7ba
André Luis <anddreluis98@gmail.com>
2022-04-27 16:30:33 -0300

    subscribe button

--- src/components/SignInButton/Index.tsx
+++ src/components/SignInButton/Index.tsx
@@ -3,7 +3,7 @@ import { FiX } from 'react-icons/fi'
 import styles from './styles.module.scss'
 
 export function SignInButton() {
-    const isUserLoggedIn = true;
+    const isUserLoggedIn = false;
 
     return isUserLoggedIn ? (
         <button
