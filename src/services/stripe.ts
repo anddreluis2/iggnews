58a724d98b00ffe07e08c2e8eacd2d5a6ecc6063
André Luis <anddreluis98@gmail.com>
2022-04-28 08:49:14 -0300

    arrumando erro da api

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,7 +1,7 @@
 import Stripe from "stripe";
 
 export const stripe = new Stripe(
-    process.env.STRIPE_APY_KEY,
+    process.env.STRIPE_API_KEY,
     {
         apiVersion: '2020-08-27',
         appInfo: {
