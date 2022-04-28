0ea9afeaa34fe26184cfd0f0906db25c0eb05a8e
André Luis <anddreluis98@gmail.com>
2022-04-28 00:12:41 -0300

    TEM A API KEY IRMAOOOOOOOOOOO PUTA QUE PARIU

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,17 @@
 import Head from "next/head";
+import { GetServerSideProps } from "next";
 import { SubscribeButton } from "../components/SubscribeButton";
 import styles from './home.module.scss'
+import { stripe } from "../services/stripe";
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string,
+    amount: number,
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -14,12 +23,29 @@ export default function Home() {
           <h1>News about the <span>React</span> world.</h1>
           <p>
             Get acess to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
           <SubscribeButton />
         </section>
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
-  )
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1KtNJ8E5HZc3py3iSn1RWXO0', {
+    expand: ['product']
+  })
+
+  const product = {
+    priceId: price.id,
+    amount: (price.unit_amount / 100),
+  }
+
+  return {
+    props: {
+      product,
+    }
+  }
 }
