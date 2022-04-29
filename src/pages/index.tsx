960e498d5a92012194cc20f56c72d7ace3b3292c
André Luis <anddreluis98@gmail.com>
2022-04-28 21:16:36 -0300

    aplicando ssg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import { SubscribeButton } from "../components/SubscribeButton";
 import styles from './home.module.scss'
 import { stripe } from "../services/stripe";
@@ -33,7 +33,7 @@ export default function Home({ product }: HomeProps) {
   );
 };
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1KtNJ8E5HZc3py3iSn1RWXO0', {
     expand: ['product']
   })
@@ -49,6 +49,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60*60*24,
   }
 }
